1de11ecba392ad81f9fb0875b9fa3ace6567d864
Ana Beatriz Rocha Duarte <83419660+AnaDuarte1@users.noreply.github.com>
2025-01-21 15:32:42 -0300

    Update DesastreNatural.java

--- project/src/com/bemtevi/model/DesastreNatural.java
+++ project/src/com/bemtevi/model/DesastreNatural.java
@@ -1,5 +1,45 @@
 package com.bemtevi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DesastreNatural {
-    // Nome do desastre
+    private String nome;
+
+    private static List<DesastreNatural> desastres = new ArrayList<>();
+
+
+    public DesastreNatural(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static void adicionarDesastre(DesastreNatural desastre) {
+        desastres.add(desastre);
+    }
+
+    public static List<DesastreNatural> listarDesastres() {
+        return desastres;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    static {
+        adicionarDesastre(new DesastreNatural("Enchente"));
+        adicionarDesastre(new DesastreNatural("Terremoto"));
+        adicionarDesastre(new DesastreNatural("Tsunami"));
+        adicionarDesastre(new DesastreNatural("Incêndio Florestal"));
+        adicionarDesastre(new DesastreNatural("Furacão"));
+    }
+    
 }
